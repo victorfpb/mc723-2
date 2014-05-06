830788e3fe7b3f6e990d9f10741904d74f7b218b
Ramon Carvalho Maciel <ra092762@beach.lab.ic.unicamp.br>
2014-05-06 17:29:18 -0300

    Vectors initiated

--- watcher.cpp
+++ watcher.cpp
@@ -1,13 +1,19 @@
 #include "watcher.h"
  
 void Watcher::start(){
+  zeroVector(rdVec, WATCHER_VEC_SIZE);
+  zeroVector(rsVec, WATCHER_VEC_SIZE);
+  zeroVector(rtVec, WATCHER_VEC_SIZE);
 }
 
 void Watcher::finish(){
 }
 
 void Watcher::registerInstruction(int rd, int rs, int rt	) {
-	checkForHazard();
+  pushToVector(rd, rdVec, WATCHER_VEC_SIZE);
+  pushToVector(rs, rsVec, WATCHER_VEC_SIZE);
+  pushToVector(rt, rtVec, WATCHER_VEC_SIZE);
+  checkForHazard();
 }
 
 void Watcher::branchInstruction(int oldpc, int newpc, int intendedPC) {
@@ -15,4 +21,17 @@ void Watcher::branchInstruction(int oldpc, int newpc, int intendedPC) {
 
 void Watcher::checkForHazard() {
 
+}
+
+void Watcher::pushToVector(int reg, int* vec, int size) {
+ for (int i=size-1; i>0; i--) {
+	vec[i-1] = vec[i];
+ }
+ vec[0] = reg;
+}
+
+void Watcher::zeroVector(int* vec, int size){
+ for (int i=0; i>size; i++) {
+	vec[0] = 0;
+ }
 }
--- watcher.h
+++ watcher.h
@@ -1,9 +1,14 @@
-#ifndef DATE_H
-#define DATE_H
+#ifndef WATCHER_H
+#define WATCHER_H
+
+#define WATCHER_VEC_SIZE 13
  
 class Watcher
 {
 private:
+   int rdVec[WATCHER_VEC_SIZE];
+   int rsVec[WATCHER_VEC_SIZE];
+   int rtVec[WATCHER_VEC_SIZE];
  
 public:
    Watcher() {}
@@ -12,6 +17,8 @@ void finish();
 void registerInstruction(int rs, int rt, int rd);
 void checkForHazard();
 void branchInstruction(int oldpc, int newpc, int intendedPC);
+void pushToVector(int reg, int* vec, int size);
+void zeroVector(int* vec, int size);
 };
  
 #endif
